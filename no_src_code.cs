8d5a8e7260e61b09c3ac6814a51345a3c3615643
wenhsian41 <51377447+wenhsian41@users.noreply.github.com>
2022-01-14 10:05:18 +0800

    Add files via upload

---
+++ no_src_code.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace na
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            
+        }
+    }
+}
